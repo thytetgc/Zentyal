e96b4f7ee6dbc2443dc9e065607b1e26eb40a9f8
Thiago Castro <41759383+thytetgc@users.noreply.github.com>
2019-12-25 17:08:18 -0300

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+clear
+
+echo [`date +'%d.%m.%Y %H:%M'`] Iniciando Backup ...
+	 echo "+-------------------------------------------------+OK"
+
+echo [`date +'%d.%m.%Y %H:%M'`] Exportar configuração atual [1/4] ...
+rm /var/lib/zentyal/conf/backups/*
+/usr/share/zentyal/global-action --action saveAllModules
+/usr/share/zentyal/make-backup --config-backup --description BKP
+	 echo "+-------------------------------------------------+OK"
+
+# Diretórios para fazer backup, /etc /root /var
+source='/etc /Scripts /usr/zentyal /var/lib/zentyal/ /var/log /opt'
+
+# Diretório de backup, /Backup
+#backup=/Backup/"`hostname`"
+backup=/Backup
+mkdir -p $backup
+
+# Definição de Variavel
+datum=$(date +'%Y%m%d')
+dateiname=$backup/"`hostname -a`"-backup$datum.tar
+
+# -----------------------------------------------------
+function f_delFiles()
+# -----------------------------------------------------
+# $1 Diretório de backup
+{
+  loeschdatum=$(date --date='7 days ago' +'%Y%m%d')
+  rm $1/"`hostname -a`"-backup$loeschdatum.tar
+}
+
+echo [`date +'%d.%m.%Y %H:%M'`] Exclundo arquivos em $backup, com mais de 7 dias [2/4] ...
+f_delFiles $backup
+	 echo "+-------------------------------------------------+OK"
+
+echo [`date +'%d.%m.%Y %H:%M'`] Salve $source em $dateiname [3/4] ...
+tar Pcf $dateiname $source
+	 echo "+-------------------------------------------------+OK"
+
+echo [`date +'%d.%m.%Y %H:%M'`] Sincronizar com armazenamento online [4/4] ...
+bash /Scripts/upload.sh
+	 echo "+-------------------------------------------------+OK"
+
+echo [`date +'%d.%m.%Y %H:%M'`] Pronto!
+	 echo "+-------------------------------------------------+OK"
